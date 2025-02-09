f5f8afb18a42e531d27b7a03a963ff5946a4f242
nab138 <nab@nabdev.me>
2025-02-08 23:09:45 -0500

    Change how natives are loaded

--- src/main/java/me/nabdev/physicsmod/PhysicsMod.java
+++ src/main/java/me/nabdev/physicsmod/PhysicsMod.java
@@ -1,7 +1,5 @@
 package me.nabdev.physicsmod;
 
-import com.github.puzzle.core.Constants;
-import com.github.puzzle.core.loader.meta.EnvType;
 import com.github.puzzle.core.loader.provider.mod.entrypoint.impls.ModInitializer;
 import com.github.puzzle.core.loader.util.ModLocator;
 import com.github.puzzle.game.PuzzleRegistries;
@@ -15,7 +13,12 @@ import me.nabdev.physicsmod.items.GravityGun;
 import me.nabdev.physicsmod.items.Linker;
 import me.nabdev.physicsmod.items.MysticalGem;
 import me.nabdev.physicsmod.items.PhysicsInfuser;
-import me.nabdev.physicsmod.utils.NativeLibraryLoader;
+import electrostatic4j.snaploader.LibraryInfo;
+import electrostatic4j.snaploader.LoadingCriterion;
+import electrostatic4j.snaploader.NativeBinaryLoader;
+import electrostatic4j.snaploader.filesystem.DirectoryPath;
+import electrostatic4j.snaploader.platform.NativeDynamicLibrary;
+import electrostatic4j.snaploader.platform.util.PlatformPredicate;
 
 import java.util.logging.Level;
 
@@ -39,8 +42,19 @@ public class PhysicsMod implements ModInitializer {
 
         Commands.register();
 
-        boolean success = NativeLibraryLoader.loadLibbulletjme("Release", "Sp", Constants.SIDE == EnvType.CLIENT);
-        if (!success) {
+        LibraryInfo info = new LibraryInfo(
+                new DirectoryPath("linux/x86-64/com/github/stephengold"),
+                "bulletjme", DirectoryPath.USER_DIR);
+        NativeBinaryLoader loader = new NativeBinaryLoader(info);
+        NativeDynamicLibrary[] libraries = new NativeDynamicLibrary[]{
+                new NativeDynamicLibrary("native/linux/x86_64", PlatformPredicate.LINUX_X86_64),
+                new NativeDynamicLibrary("native/osx/arm64", PlatformPredicate.MACOS_ARM_64),
+                new NativeDynamicLibrary("native/windows/x86_64", PlatformPredicate.WIN_X86_64)
+        };
+        loader.registerNativeLibraries(libraries).initPlatformLibrary();
+        try {
+            loader.loadLibrary(LoadingCriterion.INCREMENTAL_LOADING);
+        } catch (Exception e) {
             throw new RuntimeException("Failed to load native library. Please contact nab138, he may need to add support for your platform.");
         }
         PhysicsRigidBody.logger2.setLevel(Level.WARNING);
